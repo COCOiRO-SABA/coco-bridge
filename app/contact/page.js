57f71858a4c674ee40cc1635847f1760c2db302d
COCOiRO-SABA <megumu-tamura@coco-i-ro.com>
2026-06-27 23:37:40 +0900

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -16,7 +16,7 @@ export default function ContactPage() {
     setStatus('sending')
     setErrorMsg('')
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_GAS_URL, {
+      const res = await fetch(process.env.NEXT_PUBLIC_GAS_API, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ action: 'contact', data: form }),
