3b59263961aed448d1066370ce16761caae90655
COCOiRO-SABA <megumu-tamura@coco-i-ro.com>
2026-06-30 20:18:40 +0900

    タスク5: recruit ページのナビを /blog から /news に変更

--- app/recruit/page.js
+++ app/recruit/page.js
@@ -32,7 +32,7 @@ export default async function RecruitPage() {
           <li><a href="/services">サービス</a></li>
           <li><a href="/about">私たちについて</a></li>
           <li><a href="/staff">スタッフ</a></li>
-          <li><a href="/blog">ブログ</a></li>
+          <li><a href="/news">お知らせ</a></li>
           <li><a href="/contact">お問い合わせ</a></li>
         </ul>
       </nav>
